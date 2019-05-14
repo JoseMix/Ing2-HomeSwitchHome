21a67ebac042c6d5fa1b926a6e0f970c7878c975
JoseMi Silva <js.silva.010@gmail.com>
2019-05-14 16:46:56 -0300

    modificacion en subasta

--- iniciarsub.php
+++ iniciarsub.php
@@ -7,11 +7,42 @@ if (!$conexion){
     die();
 }
 
+$id_propiedad=$_GET[idpropiedad];
+$semana=$_GET[semana];
+$anio=_GET[anio];
+$precio
 
-$propiedades_totales = $conexion->prepare("
-    SELECT * FROM propiedades");
-$propiedades_totales->execute();
-$propiedades_totales = $propiedades_totales->fetchAll();
+
+
+
+$idsemana = $conexion->prepare("SELECT id_semana FROM fecha WHERE semana=:semana AND anio=:anio");
+$consulta_fecha->execute();
+$consulta_fecha = $consulta_fecha->fetchAll();
+
+$id_semana_propiedad = $conexion->prepare("SELECT idfecha_propiedad FROM propiedades INNER JOIN fecha 
+
+
+
+
+
+
+
+
+
+SELECT * 
+FROM table1 
+INNER JOIN table2
+      ON table1.primaryKey=table2.table1Id
+INNER JOIN table3
+      ON table1.primaryKey=table3.table1Id
+
+
+
+
+
+
+                                        ");
+
 
 
 require 'views/iniciarsub.view.php';
