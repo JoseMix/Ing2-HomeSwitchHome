81f813326f3747d333afdfe24c723f3fb760aa80
JoseMi Silva <js.silva.010@gmail.com>
2019-05-17 17:52:57 -0300

    se añadio el iniciar subasta

--- iniciarsub.php
+++ iniciarsub.php
@@ -7,58 +7,68 @@ if (!$conexion){
     die();
 }
 $errores = '';
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $preciobase = $_POST['preciobase'];
-    $fechainicio = $_POST['fecha'];
-    $fechafin = date('Y-m-d', strtotime($fechainicio . ' + 3 days'));
+    $fecha = $_POST['fecha']; //fecha reserva
+    $fechainicio1 = $_POST['inicio']; //inicio subasta
+    $fechafin = date('Y-m-d', strtotime($fechainicio1 . ' + 3 days')); //fin subasta
     $idpropiedad = 1;  //idpropiedad debe ser pasado
+    
+
+    ///diferencia de dias desde hoy hasta la fecha de la subasta
+    $hoy=date_create(date('Y-m-d')); //fecha de hoy
+    $hasta=date_create($fecha);   
+    $diff=date_diff($hoy,$hasta);
+    $fechainicio= date_create($fechainicio1);
+    $diffsubasta=date_diff($fechainicio,$hasta);
+    $dias = $diff->format('%a');
+    $diassubasta = $diffsubasta->format('%a');
+    //////CALCULO DE AÑO Y SEMANA DE RESERVA
+    $date = new DateTime($_POST['fecha']);
+    $semana = $date->format('W');
+    $anio = $date->format('Y');
+
+    if(empty($preciobase) or empty($fecha)){
+        $errores .= '<li>Por Favor rellene todos los campos</li>';
+    }else{
+
+    if (($dias < 365) or ($diassubasta < 182)){
+        $errores .= "<li>Fecha de subasta Incorrecta</li>";
+        }else{
+            $conexion = conexion();    
+            $statement=$conexion->prepare('SELECT * FROM subasta WHERE idpropiedad = :idpropiedad AND semana= :semana AND anio= :anio LIMIT 1' );
+            $statement->execute(array(
+                ':idpropiedad' => $idpropiedad,
+                ':semana' =>$semana,
+                ':anio' =>$anio
+                ));
+            $resultado = $statement->fetch();
+            if($resultado != false){
+                $errores .= '<li>Propiedad Ya Subastada en esa fecha</li>';
+            }
+        }
+            if ($errores == ''){
+                $statement = $conexion->prepare('INSERT INTO subasta (preciobase, fechainicio, fechafin, idpropiedad, semana, anio) 
+                VALUES (:preciobase, :fechainicio, :fechafin, :idpropiedad, :semana, :anio)');
+                $statement->execute(array(
+                       ':preciobase' => $preciobase,
+                        ':fechainicio' => $fechainicio1,
+                        ':fechafin' => $fechafin,
+                        ':idpropiedad' => $idpropiedad,
+                        ':semana' => $semana,
+                        ':anio' => $anio
+                    )); 
+                }
+            
+            }
+
+
+    
 
 
 
-//DIFERENCIA DE MAS DE 6 MESES EN ADELANTE Y NUMERO DE SEMANA//
 
-$fecha1 = $_POST["fecha"];
-$fecha2 = date("Y-m-d");
-$fecha3 = new Datetime($fecha1);
-$semana = $fecha3->format("W");
-$fecha1 = new Datetime($fecha1);
-$fecha2 = new Datetime($fecha2);
-$diferencia = date_diff($fecha1, $fecha2);
-$diferencia = $diferencia->format('%m');
-if ($diferencia < 6) {
-    $errores .= "<li>Fecha de subasta demasiado cerca</li>";
-}else{
-    $conexion = conexion();    
-    $statement=$conexion->prepare('SELECT * FROM subasta WHERE idpropiedad = :idpropiedad AND semana= :semana LIMIT 1' );
-    $statement->execute(array(
-        ':idpropiedad' => $idpropiedad,
-        ':semana' =>$semana   
-    ));
-   $resultado = $statement->fetch();
-    print_r ($resultado);
-    if($resultado != false){
-        $errores .= '<li>Propiedad Ya Subastada en esa fecha</li>';
-    }
-}
-if ($errores = ''){
-    $conexion = conexion();    
-    $statement = $conexion->prepare('INSERT INTO subasta (preciobase, fechainicio, fechafin, idpropiedad, semana ) 
-                                        VALUES (:preciobase, :fechainicio, :fechafin, :idpropiedad, :semana)');
-    $statement->execute(array(
-        ':preciobase' => $preciobase,
-        ':fechainicio' => $fechainicio,
-        ':fechafin' => $fechafin,
-        ':idpropiedad' => $idpropiedad,
-        ':semana' => $semana
-    )); 
-    }
-
-
-}
-
-
-
+        }
 
 
 
@@ -75,49 +85,6 @@ if ($errores = ''){
 
 
 
-/*$id_propiedad=$_GET[idpropiedad];
-
-$idsemana = $conexion->prepare("SELECT id_semana FROM fecha WHERE semana=:semana AND anio=:anio");
-$consulta_fecha->execute();
-$consulta_fecha = $consulta_fecha->fetchAll();
-
-$id_semana_propiedad = $conexion->prepare("SELECT idfecha_propiedad FROM propiedades INNER JOIN fecha 
-
-
-
-
-
-
-
-
-
-SELECT * 
-FROM table1 
-INNER JOIN table2
-      ON table1.primaryKey=table2.table1Id
-INNER JOIN table3
-      ON table1.primaryKey=table3.table1Id
-
-
-
-
-
-
-                                        ");
-*/
-
-
-
-/*CALCULO DE EL EQUIVALENTE EN FECHAS DE UNA SEMANA
-$semana=10;
-$anio =2019;
-$inicio_semana = new DateTime();
-$inicio_semana->setISODate($anio,$semana);
-echo $inicio_semana->format('d-M-Y');
-$fin_semana = new DateTime();
-$fin_semana->setISODate($anio,$semana+1);
-echo $fin_semana->format('d-M-Y');
-*/
 
 
 
@@ -125,6 +92,12 @@ echo $fin_semana->format('d-M-Y');
 
 
 require 'views/iniciarsub.view.php';
+
+
+
+
+
+
 ?>
 
 
